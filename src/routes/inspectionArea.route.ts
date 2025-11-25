898698feb28d97895ff3c416e89cea9a2f33197f
subhankhan.cloudarmors <subhankhan.cloudarmors@gmail.com>
2025-11-25 18:30:34 +0000

    inspectionArea.route.ts edited online with Bitbucket

--- src/routes/inspectionArea.route.ts
+++ src/routes/inspectionArea.route.ts
@@ -1,17 +1,41 @@
-import { createInspectionAreaController, deleteInspectionAreasController, getAllInspectionAreasController, updateInspectionAreasController } from '../controllers/inspectionArea.controller';
-import { authenticateToken } from '../middlewares/authMiddleware';
-import { validateRequest } from '../middlewares/validateRequest';
-import { createInspectionAreaSchema, deleteInspectionAreaSchema, updateInspectionAreaSchema } from '../utils/validators/inspectionAreaValidation';
-import { apiLimiter } from './../middlewares/rateLimiter';
+import {
+  createInspectionAreaController,
+  deleteInspectionAreasController,
+  updateInspectionAreasController,
+  getInspectionAreaByIDController,
+  getAllInspectionAreasStationsController,
+} from "../controllers/inspectionArea.controller";
+import { authenticateToken } from "../middlewares/authMiddleware";
+import { validateRequest } from "../middlewares/validateRequest";
+import {
+  createInspectionAreaSchema,
+  deleteInspectionAreaSchema,
+  updateInspectionAreaSchema,
+} from "../utils/validators/inspectionAreaValidation";
+import { apiLimiter } from "./../middlewares/rateLimiter";
 import { Router } from "express";
 
 const router = Router();
-router.use(apiLimiter)
-router.use(authenticateToken)
+router.use(apiLimiter);
+router.use(authenticateToken);
 
-router.post("/", validateRequest(createInspectionAreaSchema) , createInspectionAreaController);
-router.get("/", getAllInspectionAreasController);
-router.put("/:id", validateRequest(updateInspectionAreaSchema) , updateInspectionAreasController);
-router.delete("/:id", validateRequest(deleteInspectionAreaSchema) , deleteInspectionAreasController);
+router.post(
+  "/",
+  validateRequest(createInspectionAreaSchema),
+  createInspectionAreaController
+);
+router.get("/", getAllInspectionAreasStationsController);
+router.get("/:id", getInspectionAreaByIDController);
 
-export {router as inspectionAreaRoutes}
+router.put(
+  "/:id",
+  validateRequest(updateInspectionAreaSchema),
+  updateInspectionAreasController
+);
+router.delete(
+  "/:id",
+  validateRequest(deleteInspectionAreaSchema),
+  deleteInspectionAreasController
+);
+
+export { router as inspectionAreaRoutes };
